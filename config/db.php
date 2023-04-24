1131c3a10a6c22cb7abc7c3aee3c279fb38e125e
jivimuz <jivirasgal@gmail.com>
2023-04-24 10:24:19 +0700

    first commit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1:3307;dbname=siklinik3',
+    'dsn' => 'mysql:host=127.0.0.1:3307;dbname=kliniku',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
